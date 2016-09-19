ef19629f9c74d4d1a8230e17d06ef63d88af1aa6
Kelly McNevin <kelly.mcnevin@gmail.com>
2016-09-19 20:44:14 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -11,11 +11,11 @@ function callMe() {
 
 function crazy()
 {
-  var thisIsCrazy = function (){
+  var thisIsCrazy = function ()
+  {
     console.log("hey!!!")
-  thisIsCrazy();
-
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
