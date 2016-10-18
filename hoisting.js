3ee931211b2ab856854b702556bd499ce54f9255
Chelsea Hicks <chelseamariehicks@gmail.com>
2016-10-18 18:57:58 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,25 @@
-function callMe() {
+var lyric = "maybe";
+
+function callMe() {return lyric;
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
-  var lyric = "maybe";
+
 }
 
+var thisIsCrazy = function (){ console.log("hey!!!")}
 
-function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
+function crazy(){thisIsCrazy();}
 
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
-  }
-}
 
-function sayMyName() {
+function sayMyName() {return sayMy();
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
+sayMy();
 
-  function sayMy() {
+  function sayMy() {var name; name = "Kristin"
     console.log(name);
     var name = "Kristin";
-  }
-}
+  }}
