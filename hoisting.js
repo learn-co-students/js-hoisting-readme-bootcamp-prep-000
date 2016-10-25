90f24905bc58cb349a7888a773676cf6589d9185
Azekah Griffiths <azekah.griffiths@icloud.com>
2016-10-25 16:03:57 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -3,22 +3,20 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  var lyric = "maybe";
-  return lyric;
 
+  var lyric = "maybe";
+    return lyric;
 }
 
-crazy();
+
 function crazy() {
   // fix the code in here:
-
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
     thisIsCrazy();
 }
-
 
 function sayMyName() {
   // fix the code in here:
@@ -27,8 +25,8 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
-    var name = "Kristin";
-    console.log(name);
 
+    var name = "Kristin";
+        console.log(name);
   }
 }
