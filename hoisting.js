3bcd0bf431fe97d47d2088c9e334e6090eb8cb74
Seth Dornbusch <sethdylan@gmail.com>
2016-11-21 23:26:23 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,26 @@
 function callMe() {
+  var maybe = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
-  var lyric = "maybe";
+  return maybe;
 }
 
 
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
-  }
+    }
+      thisIsCrazy();
 }
 
-function sayMyName() {
-  // fix the code in here:
+
+function sayMyName(){
   var name = "Cricky";
-
-  sayMy();
-
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
+   function sayMy() {
+     var name = "Kristin";
+     console.log(name);
+    }
+    sayMy();
   }
-}
