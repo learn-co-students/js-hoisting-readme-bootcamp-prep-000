f18799a305ee38e7c2cda648f99b325d4f2c3c36
Autumn Raine <autumn.raine@icloud.com>
2016-11-30 16:52:33 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -3,28 +3,25 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  return lyric;
 }
 
 
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
+  // fix the code in here
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
-
-  sayMy();
+  // fix the code in here
 
   function sayMy() {
+    name = "Kristin";
     console.log(name);
-    var name = "Kristin";
   }
+  sayMy();
 }
