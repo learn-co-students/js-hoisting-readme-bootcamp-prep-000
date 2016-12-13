eae2866c6593b69a7c9817499bd6c1b77148e49c
Shelly Xu <xushell@gmail.com>
2016-12-13 01:29:56 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -4,12 +4,12 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+
   return lyric;
 }
 
 
 function crazy() {
-  // fix the code in here:
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
