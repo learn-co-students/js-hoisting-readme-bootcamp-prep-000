fbe4eaa3b7735024358f11c925f532f445a861d3
Leibel Hecht <leibelhecht@gmail.com>
2016-12-15 09:25:28 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,33 +1,31 @@
 function callMe() {
 var lyric = "maybe";
+return lyric;
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
 }
 
 
 function crazy() {
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
-  }
-  thisIsCrazy();
+  // fix the code in here:
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-  function sayMy() {
-var name = "Kristin";
-    console.log(name);
 
-  }
   sayMy();
 
-
+  function sayMy() {
+    var name = "Kristin";
+    console.log(name);
+  
+  }
 }
