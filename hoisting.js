640af111585c45d6ff24ee1d32b0e4c59e27ea95
Steven Hui <stevenhhui@gmail.com>
2017-01-03 04:34:57 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,6 +1,7 @@
 // Variable hoisting: Remember to define all variables at the top of the scope.
 // Define all variables at the top of your functions to access them later.
-
+// Below, since var lyric is being returned within the callMe function, it
+// does not need to be globally accessible, just within the function callMe().
 function callMe() {
   var lyric = "maybe";
   console.log("I just met you...");
@@ -10,36 +11,31 @@ function callMe() {
   return lyric;
 }
 
+// var thisIsCrazy is set to a function. A function which logs "hey!!!".
+// Instead of calling a variable inside of itself, call it outside of the
+// variable declaration, but still inside the crazy function.
 function crazy() {
-  // fix the code in here:
-  console.log("hey!!!")
-  var thisIsCrazy = function() {
-    thisIsCrazy();
-  }
-}
-/*function crazy() {
   // fix the code in here:
   var thisIsCrazy = function (){
     console.log("hey!!!");
   }
 thisIsCrazy();
-}*/
+}
 
-/*function sayMyName() {
+// When JS reads through code, it notes any variables you've defined
+// and stores the value undefined in them. It executes any console.logs
+// when it hits them. It also keeps track of any functions that are defined
+// and stores them in local memory. That is how you can call a function in
+// a file on a line above where the function is defined, like sayMy(); below.
+// When JS sees function sayMyName(), it stores the entire function in memory
+// under sayMyName. The second line that JS reads is actually the function call
+// sayMyName(). More info https://rainsoft.io/javascript-hoisting-in-details/
+function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
   sayMy();
   function sayMy() {
-var name = "Kristen";
+    var name = "Kristin";
     console.log(name);
-  }
-}*/
-function sayMyName() {
-  // fix the code in here:
-  var name = "Kristin";
-  console.log(name);
-  var name = "Cricky"
-  function sayMy() {
-    sayMy();
   }
 }
