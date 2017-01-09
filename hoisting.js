7c104d0f836c6ce02103db14150bcfffa2ff7c63
Dwayne Johnson <dwayne_j23@yahoo.com>
2017-01-09 22:55:25 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,9 +1,9 @@
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  var lyric = "maybe";
   return lyric;
 }
 
@@ -17,6 +17,7 @@ function crazy() {
 
 function sayMyName() {
   var name = "Cricky";
+  
   function sayMy() {
     var name = "Kristin";
     console.log(name);
