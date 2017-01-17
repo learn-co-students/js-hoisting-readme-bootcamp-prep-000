ed84ffd6a6608c1dc9cc94f9ef916e1969ed9b48
John Ziegner <jziegner23@gmail.com>
2017-01-17 05:39:24 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,27 @@
+var lyric;
 function callMe() {
-  console.log("I just met you...");
+
+lyric= "maybe";
+
+ console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
-  var lyric = "maybe";
+
+return lyric;
 }
 
+callMe();
 
+crazy();
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
-  var thisIsCrazy = function (){
     console.log("hey!!!")
   }
-}
 
-function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
 
-  sayMy();
-
-  function sayMy() {
+sayMyName();
+  function sayMyName() {
+  var name = "Kristin";
     console.log(name);
-    var name = "Kristin";
   }
-}
+sayMyName();
