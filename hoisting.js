05db0074fa36f0b8f7c000214e5a6808cd5d5d23
Mirian Amaya Alvarez <me.amaya234@gmail.com>
2017-01-24 23:57:00 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,9 +1,9 @@
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  var lyric = "maybe";
   return lyric;
 }
 
@@ -24,6 +24,36 @@ function sayMyName() {
 
   function sayMy() {
     var name = "Kristin";
+    console.log(name);
+  }
+}
+
+function callMe(){
+  var lyric = "maybe";
+  console.log("I just met you...");
+  console.log("and this is crazy..");
+  console.log("but here's my number..");
+  console.log("so call me");
+  return lyric;
+}
+
+function crazy(){
+  //fix the code in here:
+  var thisIsCrazy = function (){
+    console.log("hey!!!")
+  }
+  thisIsCrazy();
+}
+
+function sayMyName() {
+  // fix the code in here:
+  var name = "Cricky";
+
+  sayMy();
+
+  function sayMy() {
+    var name = "Kristin";
+
     console.log(name);
   }
 }
