738f060fc334d5a38f366ca6259d5c43c460db19
Derrick Chu <dc79627n@pace.edu>
2017-01-25 00:27:47 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,20 +1,23 @@
+var lyric = "maybe";
+
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
+  // var lyric = "maybe";
 }
 
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
+  //thisIsCrazy();
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
@@ -28,3 +31,4 @@ function sayMyName() {
     var name = "Kristin";
   }
 }
+done();
