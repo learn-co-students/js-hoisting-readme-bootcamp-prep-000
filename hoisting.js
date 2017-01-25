7c3e0bea79e495d7d8b93912e5f56a03195a0fc5
Calvin Lee <calvinkflee@gmail.com>
2017-01-25 21:34:13 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -22,7 +22,7 @@ function crazy() {
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-  function sayMy() }
+  function sayMy() {
     var name = "Kristin";
     console.log(name);
   }
