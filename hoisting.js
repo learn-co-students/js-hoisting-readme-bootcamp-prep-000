3d54a2f00af1881fc5cdd06b21d75a7da492cf3c
Roque Mesa <roque.mesa@gmail.com>
2017-01-27 03:40:16 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,21 +1,30 @@
+
+lyric = "maybe";
 function callMe() {
+  var lyric;
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
-  var lyric = "maybe";
+  return "maybe";
+
 }
+callMe ();
+
+
 
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
 
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
+var thisIsCrazy = function () {
+      console.log("hey!!!")
+      thisIsCrazy(){
+    }
+
   }
 }
+
 
 function sayMyName() {
   // fix the code in here:
