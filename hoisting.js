0d456af50159d8840004df6a4d4782f4b8d90b75
Sterling Black <sterlingblack13@gmail.com>
2017-02-06 23:36:47 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,15 +1,16 @@
+var lyric = "maybe"
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
 
 function crazy() {
-  // fix the code in here:
+  console.log("hey!!!")
+
   thisIsCrazy();
 
   var thisIsCrazy = function (){
@@ -18,7 +19,7 @@ function crazy() {
 }
 
 function sayMyName() {
-  // fix the code in here:
+  return "Kristin"
   var name = "Cricky";
 
   sayMy();
