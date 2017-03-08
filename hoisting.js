aebef61a367d97f566f7110efb6de7fcabaed4c0
k l <kappsolis@gmail.com>
2017-03-08 17:58:57 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,32 @@
 function callMe() {
 	var lyric = "maybe";
-	return lyric;
+
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-
+	return lyric;
 }
 
 function crazy() {
   // fix the code in here:
+	var thisIsCrazy = function (){
+		console.log("hey!!!")
+	}
 
   thisIsCrazy();
 }
-var thisIsCrazy = function (){
-	console.log("hey!!!")
-}
+
 
 function sayMyName() {
-    var name = "Cricky";
+  // fix the code in here:
+  var name = "Cricky";
 
-var name = "Kristin";
-    sayMy();
+  sayMy();
 
+  function sayMy() {
+    var name = "Kristin";
 
-    function sayMy() {
-        console.log(name);
-    }
+    console.log(name);
+  }
 }
