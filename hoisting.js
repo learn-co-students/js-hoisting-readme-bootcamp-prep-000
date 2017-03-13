bc81b9935bf2c206cb780352d1be11a6e69f62e7
Leslie Mollner <lamollner@gmail.com>
2017-03-13 02:04:32 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,22 +1,43 @@
 function callMe() {
+  var lyric;
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+  lyric = "maybe";
   return lyric;
-  var lyric = "maybe";
 }
-
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
-
   var thisIsCrazy = function (){
-    console.log("hey!!!")
+    console.log("hey!!!");
   }
+  thisIsCrazy();
 }
 
+/*
+function crazy() {
+  // fix the code in here:
+  var thisIsCrazy;
+  thisIsCrazy = function() {
+    console.log("hey!!!")
+  }
+  function();
+}
+
+
+function sayMyName() {
+  // fix the code in here:
+  // var name = "Cricky";
+  var name
+  function sayMy() {
+    console.log(name);
+  }
+  name = "Kristin";
+  sayMy();
+}
+*/
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
@@ -24,7 +45,8 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
+
+    name = "Kristin";
     console.log(name);
-    var name = "Kristin";
   }
 }
