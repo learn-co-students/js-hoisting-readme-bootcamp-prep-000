1ffc3f4059d2317e914235cc7de278618112b3a3
johnshelb <jsnd11@gmail.com>
2017-03-16 16:35:58 -0400

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,32 +1,31 @@
 function callMe() {
-  var lyric = "maybe";
-  return lyric;
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-
+  var lyric = "maybe";
+  return lyric;
 }
 
 
 function crazy() {
   // fix the code in here:
-
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
-  return thisIsCrazy();
+  thisIsCrazy();
+
+
 }
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-  var name = "Kristin";
 
   sayMy();
 
   function sayMy() {
+    var name = "Kristin";
     console.log(name);
   }
 }
