cd7c01b2d8213f5ea5ba5acc5e58c9285e0e327a
Christina Cornevin <ccornevin@hotmail.com>
2017-03-23 01:40:11 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,16 +1,17 @@
+var lyric;
+lyric = "maybe";
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
-}
+  }
 
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
+  return thisIsCrazy();
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
