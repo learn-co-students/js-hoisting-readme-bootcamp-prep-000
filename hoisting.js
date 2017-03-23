af82d16ae6e0ab245511c41adcae02e312a1a4a6
Demi Close <demiclose@gmail.com>
2017-03-23 21:47:23 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,25 @@
 function callMe() {
-  console.log("I just met you...");
-  console.log("and this is crazy..");
-  console.log("but here's my number..");
-  console.log("so call me");
-  return lyric;
-  var lyric = "maybe";
-}
-
-
-function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
+    var lyric = "maybe";
+    console.log("I just met you...");
+    console.log("and this is crazy..");
+    console.log("but here's my number..");
+    console.log("so call me");
+    return lyric;
   }
-}
 
-function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
 
-  sayMy();
-
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
+  function crazy() {
+    thisIsCrazy();
+   function thisIsCrazy(){
+     console.log("hey!!!");
+    }
   }
-}
+
+
+    function sayMyName(){
+      sayMy();
+    function sayMy() {
+      var name = "Kristin";
+     console.log(name);
+   }
+ }
