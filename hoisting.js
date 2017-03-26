0fa00ded826f9e93f14d1cc9327b867528ab3930
Pascale Chamberland <pascale.b.chamberland@gmail.com>
2017-03-26 22:59:01 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -3,18 +3,19 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  return lyric;
 }
 
 
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
+  // fix the code in here
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+
+  thisIsCrazy();
 }
 
 function sayMyName() {
@@ -22,9 +23,8 @@ function sayMyName() {
   var name = "Cricky";
 
   sayMy();
-
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
 }
