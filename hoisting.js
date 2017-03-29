0e162d2a8bb547d80f011f9974324f31fbc5b7c4
kasper Thomsen <kasperhthomsen@gmail.com>
2017-03-29 13:51:35 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -3,28 +3,23 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+var lyric = "maybe";
   return lyric;
-  var lyric = "maybe";
+
 }
 
 
-function crazy() {
   // fix the code in here:
-  thisIsCrazy();
+  function thisIsCrazy(){
 
-  var thisIsCrazy = function (){
     console.log("hey!!!")
+
   }
-}
+
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
+  var name = "Kristin"
+console.log(name);
 
-  sayMy();
-
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
 }
