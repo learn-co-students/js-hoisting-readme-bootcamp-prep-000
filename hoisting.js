05e07e39b884b195de6ac71b1729850c592699fe
Benedict  Segovia <benseg16@gmail.com>
2017-04-05 08:40:18 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -8,12 +8,11 @@ function callMe() {
 
 }
 
-
 function crazy() {
-  // fix the code in here:
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy()
 }
 
 function sayMyName() {
@@ -23,7 +22,7 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
 }
