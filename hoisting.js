b60c6cf7889e1b4757577eb301b7251aed3067d5
Justina Tran <justinavtran@yahoo.com>
2017-04-19 05:00:49 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,19 +1,18 @@
-function callMe() {
+function callMe(){
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
 
 function crazy() {
-  thisIsCrazy();
-
-  var thisIsCrazy = function (){
+  var thisIsCrazy = function () {
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
@@ -22,7 +21,9 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+
+    
+    console.log(name);
   }
 }
